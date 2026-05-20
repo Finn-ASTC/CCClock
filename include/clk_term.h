5e0a863da63eafc4b13537cc8c263ca9b72064d2
Finn <1716587717@qq.com>
2026-05-20 11:13:41 +0800

    完善了clk_term.h里面关于clk_cell结构体即最小渲染单元的定义，以及clk_texture的定义，接下来准备开始写实际对于texture的处理

--- include/clk_term.h
+++ include/clk_term.h
@@ -1,11 +1,41 @@
 #ifndef CLK_TERM_H
 #define CLK_TERM_H
 
+#include <stdint.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define ATTR_NONE 0x00       // 00000000 无属性
+#define ATTR_BOLD 0x01       // 00000001 加粗 (ANSI: 1)
+#define ATTR_DIM 0x02        // 00000010 暗淡 (ANSI: 2)
+#define ATTR_ITALIC 0x04     // 00000100 斜体 (ANSI: 3)
+#define ATTR_UNDERLINE 0x08  // 00001000 下划线 (ANSI: 4)
+#define ATTR_BLINK 0x10      // 00010000 闪烁 (ANSI: 5)
+#define ATTR_REVERSE 0x20    // 00100000 反显/反色 (ANSI: 7)
+#define ATTR_HIDDEN 0x40     // 01000000 隐藏 (ANSI: 8)
+#define ATTR_STRIKE 0x80     // 10000000 删除线 (ANSI: 9)
+
+typedef union {
+    struct {
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
+        uint8_t a;
+    } ch;
+    uint32_t raw;
+} Color24;
+
 typedef struct {
+    Color24 fg_color;
+    Color24 bg_color;
+    uint8_t attrs;
+} clk_cell;
+
+typedef struct {
+    int posx, posy;
+    int tex_w, tex_h;
+    clk_cell** texture;
 } clk_texture;
 
 void clk_term_init(void);
