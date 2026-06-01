3b25dcbafc7efb21a53db2e986e77c119eaf3b8d
Finn <1716587717@qq.com>
2026-06-01 20:30:27 +0800

    term新添函数，clock设计完api写好单元测试，准备开始写实现

--- src/clk_clock.c
+++ src/clk_clock.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 
 bool clk_clock_create(clk_clock* clk, const char* time_format, const char* font_path) {
-    (void)clk; (void)time_format; (void)font_path;
+    (void)clk;
+    (void)time_format;
+    (void)font_path;
     return false;
 }
 
@@ -12,12 +14,14 @@ void clk_clock_destroy(clk_clock* clk) {
 }
 
 bool clk_clock_change_time_format(clk_clock* clk, const char* new_time_format) {
-    (void)clk; (void)new_time_format;
+    (void)clk;
+    (void)new_time_format;
     return false;
 }
 
 bool clk_clock_change_font_path(clk_clock* clk, const char* new_font_path) {
-    (void)clk; (void)new_font_path;
+    (void)clk;
+    (void)new_font_path;
     return false;
 }
 
@@ -26,17 +30,23 @@ void clk_clock_update(clk_clock* clk) {
 }
 
 bool clk_clock_get_texture_size(const clk_clock* clk, int* width, int* height) {
-    (void)clk; (void)width; (void)height;
+    (void)clk;
+    (void)width;
+    (void)height;
     return false;
 }
 
 bool clk_clock_get_texture_pos(const clk_clock* clk, int* posx, int* posy) {
-    (void)clk; (void)posx; (void)posy;
+    (void)clk;
+    (void)posx;
+    (void)posy;
     return false;
 }
 
 bool clk_clock_set_texture_pos(clk_clock* clk, int posx, int posy) {
-    (void)clk; (void)posx; (void)posy;
+    (void)clk;
+    (void)posx;
+    (void)posy;
     return false;
 }
 
