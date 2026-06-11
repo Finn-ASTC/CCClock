2654afabebf6f57df1f57a94d8f7f80886f6bef9
Finn <1716587717@qq.com>
2026-06-12 01:16:41 +0800

    大致写好menu部分声明，准备写实现

--- include/clk_menu.h
+++ include/clk_menu.h
@@ -4,11 +4,15 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-#include "clk_term.h"
+#include "clk_key_io.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/* ------------------------------------------------------------------
+ *  Types
+ * ------------------------------------------------------------------ */
 
 typedef enum {
     CLK_MENU_TYPE_STR,
@@ -24,12 +28,13 @@ typedef struct {
     char* label;
     union {
         const char* s;
-        double n;
+        double d;
         bool b;
     } value;
     double min_val, max_val, step_val;
-    char** str_options;
-    int str_options_count;
+    char** options;
+    int option_count;
+    int option_idx;
 } clk_menu_item;
 
 typedef struct {
@@ -38,44 +43,60 @@ typedef struct {
     clk_menu_item** items;
     size_t item_count;
     size_t item_capacity;
+    int active_item;
 } clk_menu_tab;
 
 typedef struct {
     clk_menu_tab** tabs;
     size_t tab_count;
     size_t tab_capacity;
-    int active_tab_idx;
-    int active_item_idx;
+    int active_tab;
+    int scroll_offset;
     int posx, posy;
-    size_t width, height;
+    int width, height;
     bool visible;
 } clk_menu;
 
+/* ------------------------------------------------------------------
+ *  Events
+ * ------------------------------------------------------------------ */
+
 typedef enum {
     CLK_MENU_EVENT_NONE,
-    CLK_MENU_EVENT_VALUE_CHANGE,
+    CLK_MENU_EVENT_VALUE_CHANGED,
     CLK_MENU_EVENT_SUBMIT,
 } clk_menu_event_type;
 
 typedef struct {
-    clk_menu_event_type event;
+    clk_menu_event_type type;
     int tab_id;
     int item_id;
     union {
         const char* s;
-        double n;
+        double d;
         bool b;
     } value;
 } clk_menu_event;
 
+/* ------------------------------------------------------------------
+ *  Lifecycle
+ * ------------------------------------------------------------------ */
+
 clk_menu* clk_menu_create(void);
+void clk_menu_destroy(clk_menu* m);
 
-void clk_menu_destory(clk_menu* menu);
+/* ------------------------------------------------------------------
+ *  Tabs
+ * ------------------------------------------------------------------ */
 
-int clk_menu_add_tab(clk_menu* menu, const char* tab_name);
+int clk_menu_add_tab(clk_menu* m, const char* name);
 
-void clk_menu_add_item_str(clk_menu* m, int tab_id, int item_id, const char* label,
-                           int default_val_idx, const char** options, int option_count);
+/* ------------------------------------------------------------------
+ *  Items
+ * ------------------------------------------------------------------ */
+
+void clk_menu_add_item_str(clk_menu* m, int tab_id, int item_id, const char* label, int default_idx,
+                           const char** options, int option_count);
 
 void clk_menu_add_item_int(clk_menu* m, int tab_id, int item_id, const char* label,
                            double default_val, double min_val, double max_val, double step_val);
@@ -87,8 +108,45 @@ void clk_menu_add_item_action(clk_menu* m, int tab_id, int item_id, const char* 
 
 void clk_menu_remove_item(clk_menu* m, int tab_id, int item_id);
 
+/* ------------------------------------------------------------------
+ *  Interaction
+ * ------------------------------------------------------------------ */
+
+clk_menu_event clk_menu_handle_input(clk_menu* m, clk_key_event input);
+
+/* ------------------------------------------------------------------
+ *  Layout
+ * ------------------------------------------------------------------ */
+
+void clk_menu_set_position(clk_menu* m, int x, int y);
+void clk_menu_set_size(clk_menu* m, int w, int h);
+void clk_menu_set_visible(clk_menu* m, bool v);
+
+/* ------------------------------------------------------------------
+ *  External sync
+ * ------------------------------------------------------------------ */
+
+void clk_menu_set_value_str(clk_menu* m, int tab_id, int item_id, const char* val);
+void clk_menu_set_value_int(clk_menu* m, int tab_id, int item_id, double val);
+void clk_menu_set_value_bool(clk_menu* m, int tab_id, int item_id, bool val);
+
+/* ------------------------------------------------------------------
+ *  Dynamic options
+ * ------------------------------------------------------------------ */
+
+void clk_menu_add_option(clk_menu* m, int tab_id, int item_id, const char* opt);
+void clk_menu_remove_option(clk_menu* m, int tab_id, int item_id, int idx);
+void clk_menu_clear_options(clk_menu* m, int tab_id, int item_id);
+
+/* ------------------------------------------------------------------
+ *  INT range
+ * ------------------------------------------------------------------ */
+
+void clk_menu_set_item_range(clk_menu* m, int tab_id, int item_id, double min_val, double max_val,
+                             double step_val);
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif  // CLK_MENU_H
+#endif /* CLK_MENU_H */
--- src/clk_menu.c
+++ src/clk_menu.c
@@ -1,12 +1,146 @@
 #include "clk_menu.h"
 
-void clk_destroy_menu(clk_menu* menu) {
-    // todo
+#include <stdlib.h>
+#include <string.h>
+
+/* ------------------------------------------------------------------
+ *  Internal helper
+ * ------------------------------------------------------------------ */
+
+/** Find an item by tab_id + item_id. Returns NULL if not found. */
+static clk_menu_item* find_item(clk_menu* m, int tab_id, int item_id) {
+    for (size_t ti = 0; ti < m->tab_count; ++ti) {
+        if (m->tabs[ti]->id != tab_id)
+            continue;
+        for (size_t ii = 0; ii < m->tabs[ti]->item_count; ++ii) {
+            if (m->tabs[ti]->items[ii]->id == item_id)
+                return m->tabs[ti]->items[ii];
+        }
+    }
+    return NULL;
 }
 
-clk_texture clk_menu_to_texture(const clk_menu* menu) {
-    // todo
+/* ------------------------------------------------------------------
+ *  Lifecycle
+ * ------------------------------------------------------------------ */
 
-    clk_texture res = {};
-    return res;
+clk_menu* clk_menu_create(void) {
+    /* TODO: allocate clk_menu, zero-init, return */
+    return NULL;
+}
+
+void clk_menu_destroy(clk_menu* m) {
+    /* TODO: free all tabs + items + option strings, then free m */
+}
+
+/* ------------------------------------------------------------------
+ *  Tabs
+ * ------------------------------------------------------------------ */
+
+int clk_menu_add_tab(clk_menu* m, const char* name) {
+    /* TODO: allocate clk_menu_tab, set id (auto-increment), strdup name, add to tabs[] */
+    return -1;
+}
+
+/* ------------------------------------------------------------------
+ *  Items
+ * ------------------------------------------------------------------ */
+
+void clk_menu_add_item_str(clk_menu* m, int tab_id, int item_id, const char* label, int default_idx,
+                           const char** options, int option_count) {
+    /* TODO: find tab, allocate item, set STR fields, clamp option_idx, add to tab->items[] */
+}
+
+void clk_menu_add_item_int(clk_menu* m, int tab_id, int item_id, const char* label,
+                           double default_val, double min_val, double max_val, double step_val) {
+    /* TODO: find tab, allocate item, set INT fields, clamp value to [min,max], add to tab->items[]
+     */
+}
+
+void clk_menu_add_item_bool(clk_menu* m, int tab_id, int item_id, const char* label,
+                            bool default_val) {
+    /* TODO: find tab, allocate item, set BOOL fields, add to tab->items[] */
+}
+
+void clk_menu_add_item_action(clk_menu* m, int tab_id, int item_id, const char* label) {
+    /* TODO: find tab, allocate item, set ACTION fields, add to tab->items[] */
+}
+
+void clk_menu_remove_item(clk_menu* m, int tab_id, int item_id) {
+    /* TODO: find tab, find item index, free item + label + options, shift array, NULL out */
+}
+
+/* ------------------------------------------------------------------
+ *  Interaction
+ * ------------------------------------------------------------------ */
+
+clk_menu_event clk_menu_handle_input(clk_menu* m, clk_key_event input) {
+    /* TODO:
+     *   visible=false → return NONE
+     *   switch(input.key):
+     *     UP/DOWN   → move active_item (update scroll_offset)
+     *     LEFT/RIGHT→ modify value (INT: ±step, BOOL: flip, STR: cycle option_idx)
+     *     TAB       → cycle active_tab
+     *     ENTER     → if ACTION type → return SUBMIT; else NONE
+     *   emit VALUE_CHANGED event when value actually changes
+     */
+    clk_menu_event ev = {.type = CLK_MENU_EVENT_NONE};
+    return ev;
+}
+
+/* ------------------------------------------------------------------
+ *  Layout
+ * ------------------------------------------------------------------ */
+
+void clk_menu_set_position(clk_menu* m, int x, int y) {
+    /* TODO: set m->posx, m->posy */
+}
+
+void clk_menu_set_size(clk_menu* m, int w, int h) {
+    /* TODO: set m->width, m->height */
+}
+
+void clk_menu_set_visible(clk_menu* m, bool v) {
+    /* TODO: set m->visible */
+}
+
+/* ------------------------------------------------------------------
+ *  External sync
+ * ------------------------------------------------------------------ */
+
+void clk_menu_set_value_str(clk_menu* m, int tab_id, int item_id, const char* val) {
+    /* TODO: find item, find val in options[], set option_idx, update value.s */
+}
+
+void clk_menu_set_value_int(clk_menu* m, int tab_id, int item_id, double val) {
+    /* TODO: find item, clamp to [min,max], set value.d */
+}
+
+void clk_menu_set_value_bool(clk_menu* m, int tab_id, int item_id, bool val) {
+    /* TODO: find item, set value.b */
+}
+
+/* ------------------------------------------------------------------
+ *  Dynamic options
+ * ------------------------------------------------------------------ */
+
+void clk_menu_add_option(clk_menu* m, int tab_id, int item_id, const char* opt) {
+    /* TODO: find item, realloc options[], strdup opt, increment option_count */
+}
+
+void clk_menu_remove_option(clk_menu* m, int tab_id, int item_id, int idx) {
+    /* TODO: find item, free options[idx], shift array, decrement option_count, clamp option_idx */
+}
+
+void clk_menu_clear_options(clk_menu* m, int tab_id, int item_id) {
+    /* TODO: find item, free all options[], set option_count=0, option_idx=0 */
+}
+
+/* ------------------------------------------------------------------
+ *  INT range
+ * ------------------------------------------------------------------ */
+
+void clk_menu_set_item_range(clk_menu* m, int tab_id, int item_id, double min_val, double max_val,
+                             double step_val) {
+    /* TODO: find item, set min/max/step, clamp current value.d into new range */
 }
